d836c72e79ba16c629eda59bbbc2235d307033ea
myrthew00 <willemsenmyrthe@gmail.com>
2022-06-21 13:29:01 +0200

    rule ll

--- workflow/rules/denovo.smk
+++ workflow/rules/denovo.smk
@@ -1,18 +1,3 @@
-# rule merge:
-#     input:
-#         r1= expand(lambda wildcards: samples.at[wildcards.sample, 'fq1'], sample = IDS) if config["skip_trimming"]=='True' else expand("../results/fastq/trimmed/{sample}_r1_P.fastq.gz", sample = IDS),
-#         r2= expand(lambda wildcards: samples.at[wildcards.sample, 'fq2'], sample = IDS) if config["skip_trimming"]=='True' else expand("../results/fastq/trimmed/{sample}_r2_P.fastq.gz", sample = IDS),
-#     output:
-#         r1 = "../results/fastq/merged/merged_r1.fq",
-#         r2 = "../results/fastq/merged/merged_r2.fq" # has to be fq file for trinity
-#     log:
-#         "../results/logs/denovo/merge.log"
-#     conda:
-#         "../envs/trinity.yaml"
-#     script:
-#         "../scripts/merge.py"
-
-
 rule merge:
     input:
         r1= expand(lambda wildcards: samples.at[wildcards.sample, 'fq1'], sample = IDS) if config["skip_trimming"]=='True' else expand("../results/fastq/trimmed/{sample}_r1_P.fastq.gz", sample = IDS),
--- workflow/scripts/merge.py
+++ workflow/scripts/merge.py
@@ -2,6 +2,20 @@ import gzip
 from Bio import SeqIO, bgzf
 # inspiration from https://bioinformatics.stackexchange.com/questions/892/how-do-you-write-a-gz-fastq-file-with-biopython
 
+# rule merge:
+#     input:
+#         r1= expand(lambda wildcards: samples.at[wildcards.sample, 'fq1'], sample = IDS) if config["skip_trimming"]=='True' else expand("../results/fastq/trimmed/{sample}_r1_P.fastq.gz", sample = IDS),
+#         r2= expand(lambda wildcards: samples.at[wildcards.sample, 'fq2'], sample = IDS) if config["skip_trimming"]=='True' else expand("../results/fastq/trimmed/{sample}_r2_P.fastq.gz", sample = IDS),
+#     output:
+#         r1 = "../results/fastq/merged/merged_r1.fq",
+#         r2 = "../results/fastq/merged/merged_r2.fq" # has to be fq file for trinity
+#     log:
+#         "../results/logs/denovo/merge.log"
+#     conda:
+#         "../envs/trinity.yaml"
+#     script:
+#         "../scripts/merge.py"
+
 def merge(in_paths, out_path):
     out_content = " "
     for in_path in in_paths:
